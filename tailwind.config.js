ee98def63be7fffa9b80fea58a7d21a8d7d035f7
Kendy <kndysong@gmail.com>
2024-11-15 20:45:31 +0100

    Change fcv img

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        
+      }
+    },
   },
   plugins: [
     require('flowbite/plugin')
